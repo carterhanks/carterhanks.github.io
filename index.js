ff2a77f7e8e6bae14f07432b38dbad6b9395d5ae
carterhanks <carterhanks@gmail.com>
2021-07-09 13:27:35 -0600

    Reverted to Previous Code

--- index.js
+++ index.js
@@ -1,44 +1,3 @@
-// Contact Form
-
-// const contactForm = document.querySelector('.contact-form');
-
-// let nameField = document.getElementById('name');
-// let email = document.getElementById('email');
-// let subject = document.getElementById('subject');
-// let message = document.getElementById('message');
-
-// contactForm.addEventListener('submit', function(e) {
-//     e.preventDefault();
-
-//     let formData = {
-//         name: nameField.value,
-//         email: email.value,
-//         subject: subject.value,
-//         message: message.value
-//     }
-
-//     // console.log(formData)
-
-//     let xhr = new XMLHttpRequest();
-//     xhr.open('POST', './');
-//     xhr.setRequestHeader('content-type', 'application/json');
-//     xhr.onload = function() {
-//         console.log(xhr.responseText);
-//         if(xhr.responseText == 'success') {
-//             alert('Email Sent!');
-//             nameField.value = '';
-//             email.value = '';
-//             subject.value = '';
-//             message.value = '';
-//         } else {
-//             alert('Something went wrong.');
-//         };
-//     };
-
-//     xhr.send(JSON.stringify(formData));
-
-// });
-
 
 // Contact Me Button Functionality
 
@@ -99,3 +58,44 @@ hireMeBtn.onclick = function(e) {
     e.preventDefault();
     window.open("https://calendly.com/carterhanks");
 };
+
+    // Contact Form
+    
+    // const contactForm = document.querySelector('.contact-form');
+    
+    // let nameField = document.getElementById('name');
+    // let email = document.getElementById('email');
+    // let subject = document.getElementById('subject');
+    // let message = document.getElementById('message');
+    
+    // contactForm.addEventListener('submit', function(e) {
+    //     e.preventDefault();
+    
+    //     let formData = {
+    //         name: nameField.value,
+    //         email: email.value,
+    //         subject: subject.value,
+    //         message: message.value
+    //     }
+    
+    //     // console.log(formData)
+    
+    //     let xhr = new XMLHttpRequest();
+    //     xhr.open('POST', './');
+    //     xhr.setRequestHeader('content-type', 'application/json');
+    //     xhr.onload = function() {
+    //         console.log(xhr.responseText);
+    //         if(xhr.responseText == 'success') {
+    //             alert('Email Sent!');
+    //             nameField.value = '';
+    //             email.value = '';
+    //             subject.value = '';
+    //             message.value = '';
+    //         } else {
+    //             alert('Something went wrong.');
+    //         };
+    //     };
+    
+    //     xhr.send(JSON.stringify(formData));
+    
+    // });
