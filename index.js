4af6150eecf656a1fb14424f78c3fd2da30db0fe
carterhanks <carterhanks@gmail.com>
2021-07-09 13:04:58 -0600

    Reverting

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 // Contact Form
 
-const contactForm = document.querySelector('.contact-form');
+// const contactForm = document.querySelector('.contact-form');
 
-let nameField = document.getElementById('name');
-let email = document.getElementById('email');
-let subject = document.getElementById('subject');
-let message = document.getElementById('message');
+// let nameField = document.getElementById('name');
+// let email = document.getElementById('email');
+// let subject = document.getElementById('subject');
+// let message = document.getElementById('message');
 
-contactForm.addEventListener('submit', function(e) {
-    e.preventDefault();
+// contactForm.addEventListener('submit', function(e) {
+//     e.preventDefault();
 
-    let formData = {
-        name: nameField.value,
-        email: email.value,
-        subject: subject.value,
-        message: message.value
-    }
+//     let formData = {
+//         name: nameField.value,
+//         email: email.value,
+//         subject: subject.value,
+//         message: message.value
+//     }
 
-    // console.log(formData)
+//     // console.log(formData)
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', './');
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload = function() {
-        console.log(xhr.responseText);
-        if(xhr.responseText == 'success') {
-            alert('Email Sent!');
-            nameField.value = '';
-            email.value = '';
-            subject.value = '';
-            message.value = '';
-        } else {
-            alert('Something went wrong.');
-        };
-    };
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('POST', './');
+//     xhr.setRequestHeader('content-type', 'application/json');
+//     xhr.onload = function() {
+//         console.log(xhr.responseText);
+//         if(xhr.responseText == 'success') {
+//             alert('Email Sent!');
+//             nameField.value = '';
+//             email.value = '';
+//             subject.value = '';
+//             message.value = '';
+//         } else {
+//             alert('Something went wrong.');
+//         };
+//     };
 
-    xhr.send(JSON.stringify(formData));
+//     xhr.send(JSON.stringify(formData));
 
-});
+// });
 
 
 // Contact Me Button Functionality
--- server.js
+++ server.js
@@ -1,48 +1,48 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const nodemailer = require('nodemailer');
-const apiKey = process.env.API_KEY;
+// require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// const nodemailer = require('nodemailer');
+// const apiKey = process.env.API_KEY;
 
-const PORT = process.env.port || 5000;
+// const PORT = process.env.port || 5000;
 
-app.use(express.static('./'))
-app.use(express.json());
+// app.use(express.static('./'))
+// app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + './index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + './index.html');
+// });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+// app.post('/', (req, res) => {
+//     console.log(req.body);
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user:'carterthatcreator@gmail.com',
-            pass: apiKey
-        }
-    });
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user:'carterthatcreator@gmail.com',
+//             pass: apiKey
+//         }
+//     });
 
-    const mailOptions = {
-        from: req.body.email,
-        to: 'carterhanks@gmail.com',
-        message: req.body.message
-    };
+//     const mailOptions = {
+//         from: req.body.email,
+//         to: 'carterhanks@gmail.com',
+//         message: req.body.message
+//     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if(error) {
-            console.log(error);
-            res.send('error');
-        } else {
-            console.log('Email Sent: ' + info.response);
-            res.send('success');
-        };
-    });
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if(error) {
+//             console.log(error);
+//             res.send('error');
+//         } else {
+//             console.log('Email Sent: ' + info.response);
+//             res.send('success');
+//         };
+//     });
 
-});
+// });
 
 
-app.listen(PORT, function() {
-    console.log(`Server Slappin on ${PORT}`);
-});
+// app.listen(PORT, function() {
+//     console.log(`Server Slappin on ${PORT}`);
+// });
